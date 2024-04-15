c459bf14bf3e788a1ff2cf1c2519a7cc2cb6b251
Daiyan Mohammad Shams <106404908+DaiyanMS@users.noreply.github.com>
2024-04-15 22:54:08 +0600

    2nd

--- Tradefair/src/tradefair/POC/StallInformationController.java
+++ Tradefair/src/tradefair/POC/StallInformationController.java
@@ -5,11 +5,17 @@
  */
 package tradefair.POC;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -27,7 +33,12 @@ public class StallInformationController implements Initializable {
     }    
 
     @FXML
-    private void back(ActionEvent event) {
+    private void back(ActionEvent event)throws IOException {
+        Parent scene2Parent = FXMLLoader.load(getClass().getResource("POC Dashboard.fxml"));
+        Scene scene2 = new Scene(scene2Parent);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(scene2);
+        window.show();
     }
     
 }
--- Tradefair/src/tradefair/manager/EmployeeInformatioController.java
+++ Tradefair/src/tradefair/manager/EmployeeInformatioController.java
@@ -5,15 +5,21 @@
  */
 package tradefair.manager;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -79,6 +85,17 @@ public class EmployeeInformatioController implements Initializable {
 
     @FXML
     private void status(ActionEvent event) {
+    }
+
+    @FXML
+    private void back(ActionEvent event)throws IOException {
+        
+         Parent scene2Parent = FXMLLoader.load(getClass().getResource("Dashboard.fxml"));
+        Scene scene2 = new Scene(scene2Parent);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(scene2);
+        window.show(); 
+
     }
     
 }
